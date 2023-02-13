595e54326c2f384f955838b193e8c4f6ad33e3f0
Appleye <zhaozb1994@outlook.com>
2023-02-13 22:08:44 +0800

    设置 User-Agent,模拟浏览器行为，避免被服务器识别为爬虫

--- reptile/src/main/java/com/appleye/utils/ReptileUtils.java
+++ reptile/src/main/java/com/appleye/utils/ReptileUtils.java
@@ -1,5 +1,6 @@
 package com.appleye.utils;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -8,9 +9,11 @@ import java.io.IOException;
 
 public class ReptileUtils {
     public static Document getDocument(String url) {
-        Document document = null;
+        Document document;
         try {
-            document = Jsoup.connect(url).get();
+            Connection connect = Jsoup.connect(url);
+            connect.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+            document = connect.get();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
